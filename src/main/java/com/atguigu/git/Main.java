66d6735cc605d170e61cc6426354525ad708b95e
LvHuaEr <hua18334804941@163.com>
2024-02-22 21:22:49 +0800

    第3次测试git3 commit

--- src/main/java/com/atguigu/git/Main.java
+++ src/main/java/com/atguigu/git/Main.java
@@ -17,6 +17,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("第一次测试git");
         System.out.printf("第二次测试git2");
+        System.out.printf("第三次测试git3");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
