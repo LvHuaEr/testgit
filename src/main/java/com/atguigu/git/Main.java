660889d07c5688620425d33ffaa8b5182c8135e2
LvHuaEr <hua18334804941@163.com>
2024-02-22 21:21:05 +0800

    第一次测试git commit

---
+++ src/main/java/com/atguigu/git/Main.java
@@ -0,0 +1,26 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author: 史俊伟
+ * @Create：2024/2/22 - 21:18
+ * @Version: v1.0
+ */    //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        System.out.printf("第一次测试git");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
